6828ed09a8c4c096fdce7b5a78de2146a84228e9
Matt <mguest55@yahoo.com>
2018-03-16 14:01:20 -0700

    Added RepoViewVC to update the current vc file.

---
+++ RepoViewer/RepoViewer/Controller/RepoViewVC.swift
@@ -0,0 +1,35 @@
+//
+//  RepoViewVC.swift
+//  RepoViewer
+//
+//  Created by Matt Guest on 3/16/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import UIKit
+
+class RepoViewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
