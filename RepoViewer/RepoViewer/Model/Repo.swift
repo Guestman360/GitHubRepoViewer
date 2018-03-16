0236a60c3500e5a120a4f7c5c4be96f910698b5d
Matt <mguest55@yahoo.com>
2018-03-16 14:11:43 -0700

    Added initial files to all new groups in project structure.

---
+++ RepoViewer/RepoViewer/Model/Repo.swift
@@ -0,0 +1,9 @@
+//
+//  Repo.swift
+//  RepoViewer
+//
+//  Created by Matt Guest on 3/16/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import Foundation
---
+++ RepoViewer/RepoViewer/View/Cells/RepoCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepoCell.swift
+//  RepoViewer
+//
+//  Created by Matt Guest on 3/16/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import UIKit
+
+class RepoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
