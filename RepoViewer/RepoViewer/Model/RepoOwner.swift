b895336c83bb2507b5a13671bafe5af20a3c245b
Matt <mguest55@yahoo.com>
2018-03-16 17:43:37 -0700

    Added RepoOwner model and Helper group.

---
+++ RepoViewer/RepoViewer/Model/RepoOwner.swift
@@ -0,0 +1,15 @@
+//
+//  RepoOwner.swift
+//  RepoViewer
+//
+//  Created by Matt Guest on 3/16/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import Foundation
+
+class RepoOwner {
+    
+    
+    
+}
