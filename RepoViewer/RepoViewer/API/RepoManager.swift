fa9533291a880234c7fc47e18f8bf513bb497ff6
Matt <mguest55@yahoo.com>
2018-03-16 22:25:18 -0700

    Got the request and everything working!

--- RepoViewer/RepoViewer/API/RepoManager.swift
+++ RepoViewer/RepoViewer/API/RepoManager.swift
@@ -9,22 +9,22 @@
 import UIKit
 
 // Enums to handle successes and failures of fethcing and loading data
-enum RepositoryFetchingError: Error {
+enum RepoFetchingError: Error {
     case loadingError
     case loadingLimitsError
     case notFound
     case mappingError
 }
 
-enum RepositoryLoadingResultType {
+enum RepoLoadingResultType {
     case success(data: Data)
     case partialSuccess(data: Data)
-    case error(error: RepositoryFetchingError)
+    case error(error: RepoFetchingError)
 }
 
-enum RepositoryFetchingResultType {
+enum RepoFetchingResultType {
     case success(repository: [LanguageForRepo])
-    case error(error: RepositoryFetchingError)
+    case error(error: RepoFetchingError)
 }
 
 class RepoManager {
@@ -34,10 +34,10 @@ class RepoManager {
     
     // MARK: - Data management
     
-    func fetchRepositories(ownerName: String, forceLoad: Bool, completion: ((RepositoryFetchingResultType) -> Void)?) {
+    func fetchRepos(ownerName: String, forceLoad: Bool, completion: ((RepoFetchingResultType) -> Void)?) {
         if forceLoad == true || repoOwner?.languageForRepo == nil {
             repoOwner = RepoOwner(name: ownerName)
-            guard let owner = repoOwner else { completion?(.error(error: RepositoryFetchingError.mappingError)); return }
+            guard let owner = repoOwner else { completion?(.error(error: RepoFetchingError.mappingError)); return }
             var repoDict = [String: [Repo]]()
             loadPage(1, name: owner.name) { result in
                 switch result {
@@ -45,27 +45,27 @@ class RepoManager {
                     do {
                         try self.mapReposTree(json, repo: &repoDict)
                     } catch {
-                        completion?(.error(error: RepositoryFetchingError.mappingError))
+                        completion?(.error(error: RepoFetchingError.mappingError))
                     }
                 case .success(let json):
                     do {
                         try self.mapReposTree(json, repo: &repoDict)
                     } catch {
-                        completion?(.error(error: RepositoryFetchingError.mappingError))
+                        completion?(.error(error: RepoFetchingError.mappingError))
                     }
                     
                     let sortedLanguages = repoDict.keys.sorted { (lang1, lang2) -> Bool in
                         repoDict[lang1]?.count ?? 0 > repoDict[lang2]?.count ?? 0
                     }
                     for language in sortedLanguages {
-                        if let owner = self.repoOwner, let repositories = repoDict[language] {
+                        if let owner = self.repoOwner, let repos = repoDict[language] {
                             if owner.languageForRepo == nil {
                                 owner.languageForRepo = [LanguageForRepo]()
                             }
-                            let sortedRepo = repositories.sorted(by: { repository1, repository2 -> Bool in
-                                repository1.starsCount ?? 0 > repository2.starsCount ?? 0
+                            let sortedRepo = repos.sorted(by: { repo1, repo2 -> Bool in
+                                repo1.starsCount ?? 0 > repo2.starsCount ?? 0
                             })
-                            owner.languageForRepo?.append((language: language, repositories: sortedRepo) as! LanguageForRepo)
+                            self.repoOwner?.languageForRepo?.append((language: language, repos: sortedRepo))
                         }
                     }
                     if let languageForRepo = self.repoOwner?.languageForRepo {
@@ -88,10 +88,10 @@ class RepoManager {
     
     func mapReposTree(_ data: Data, repo: inout [String: [Repo]]) throws {
         
-        guard let json = try? JSONSerialization.jsonObject(with: data) else { throw RepositoryFetchingError.mappingError }
-        guard let jsonarray = json as? [Any] else { throw RepositoryFetchingError.mappingError }
+        guard let json = try? JSONSerialization.jsonObject(with: data) else { throw RepoFetchingError.mappingError }
+        guard let jsonarray = json as? [Any] else { throw RepoFetchingError.mappingError }
         for item in jsonarray {
-            guard let item = item as? [String: Any] else { throw RepositoryFetchingError.mappingError }
+            guard let item = item as? [String: Any] else { throw RepoFetchingError.mappingError }
             if let newRepo = Repo(json: item) {
                 var language = NSLocalizedString("Other", comment: "")
                 if let repoLang = newRepo.language {
@@ -103,7 +103,7 @@ class RepoManager {
                     repo[language] = [newRepo]
                 }
             } else {
-                throw RepositoryFetchingError.mappingError
+                throw RepoFetchingError.mappingError
             }
         }
     }
@@ -112,18 +112,18 @@ class RepoManager {
     
     private weak var currentSessionTask: URLSessionDataTask?
     
-    func loadPage(_ page: Int, name: String, completion: ((RepositoryLoadingResultType) -> Void)?) {
+    func loadPage(_ page: Int, name: String, completion: ((RepoLoadingResultType) -> Void)?) {
         let request = PageRequest()
         request.path = "/users/\(name)/repos"
         request.page = page
         guard let url = request.url else { fatalError("can't construct url") }
         
         currentSessionTask = URLSession.shared.dataTask(with: url) { (pageData, response, error) in
-            guard let pageData: Data = pageData, let _: URLResponse = response, error == nil else { completion?(.error(error: RepositoryFetchingError.loadingError)); return }
-            guard let httpResponse = response as? HTTPURLResponse else { completion?(.error(error: RepositoryFetchingError.loadingError)); return }
-            guard httpResponse.statusCode != 404 else { completion?(.error(error: RepositoryFetchingError.notFound)); return }
-            guard httpResponse.statusCode != 403 else { completion?(.error(error: RepositoryFetchingError.loadingLimitsError)); return }
-            guard httpResponse.statusCode == 200 else { completion?(.error(error: RepositoryFetchingError.loadingError)); return }
+            guard let pageData: Data = pageData, let _: URLResponse = response, error == nil else { completion?(.error(error: RepoFetchingError.loadingError)); return }
+            guard let httpResponse = response as? HTTPURLResponse else { completion?(.error(error: RepoFetchingError.loadingError)); return }
+            guard httpResponse.statusCode != 404 else { completion?(.error(error: RepoFetchingError.notFound)); return }
+            guard httpResponse.statusCode != 403 else { completion?(.error(error: RepoFetchingError.loadingLimitsError)); return }
+            guard httpResponse.statusCode == 200 else { completion?(.error(error: RepoFetchingError.loadingError)); return }
             
             var hasMorePages = false
             if let linkString = httpResponse.allHeaderFields["Link"] as? String {
@@ -134,10 +134,10 @@ class RepoManager {
             }
             
             if hasMorePages {
-                completion?(RepositoryLoadingResultType.partialSuccess(data: pageData))
+                completion?(RepoLoadingResultType.partialSuccess(data: pageData))
                 self.loadPage(page + 1, name: name, completion: completion)
             } else {
-                completion?(RepositoryLoadingResultType.success(data: pageData))
+                completion?(RepoLoadingResultType.success(data: pageData))
             }
             
         }
@@ -145,10 +145,42 @@ class RepoManager {
         currentSessionTask?.resume()
     }
     
-    /// Closes last executed session task
+    // Closes last executed session task
     func closeCurrentSessionTaskIfNeeded() {
         currentSessionTask?.cancel()
         currentSessionTask = nil
+    }
+    
+}
+
+// Purpose of extension is to add 4 methods to help with getting language count and with indexing
+// Helpful for UITableview delegate methods
+extension RepoManager {
+    
+    func numberOfLanguages() -> Int {
+        guard let count = repoOwner?.languageForRepo?.count else { return 0 }
+        return count
+    }
+    
+    func languageAtIndex(_ index: Int) -> String? {
+        guard let repoTree = repoOwner?.languageForRepo else { return nil }
+        guard repoTree.count > index else { fatalError("index can't be more of languages amount") }
+        return repoTree[index].language
+    }
+    
+    func numberOfReposForLanguageAtIndex(_ index: Int) -> Int {
+        guard let repoTree = repoOwner?.languageForRepo else { return 0 }
+        guard repoTree.count > index else { fatalError("can't check number of repositoryes for out of bounds language index") }
+        let repos = repoTree[index].repos
+        return repos.count
+    }
+    
+    func repoForIndexPath(_ indexPath: IndexPath) -> Repo? {
+        guard let repoTree = repoOwner?.languageForRepo else { return nil }
+        guard repoTree.count > indexPath.section else { fatalError("") }
+        let repos = repoTree[indexPath.section].repos
+        guard repos.count > indexPath.row else { fatalError("") }
+        return repos[indexPath.row]
     }
     
 }
--- RepoViewer/RepoViewer/Controller/RepoViewVC.swift
+++ RepoViewer/RepoViewer/Controller/RepoViewVC.swift
@@ -23,6 +23,10 @@ class RepoViewVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchBar.delegate = self
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         tableView.tableFooterView = UIView(frame: .zero)
         // Create programmatically later
 //        activityIndicator.center = view.center
@@ -47,35 +51,91 @@ class RepoViewVC: UIViewController {
         tableView.reloadData()
     }
     
+    // Just for convenience
+    func noOwnerFoundAlert() {
+        let alertController = UIAlertController(title: NSLocalizedString("Empty owner name", comment: ""), message: NSLocalizedString("", comment: ""), preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
     // create a card view for cells??
     // http://www.kaleidosblog.com/swift-cache-how-to-download-and-cache-data-in-ios
     
     func searchReposForOwnerWithName(text: String?) {
-        
+        manager.closeCurrentSessionTaskIfNeeded()
+        if let text = text {
+            manager.fetchRepos(ownerName: text, forceLoad: true) { [weak self] result in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success:
+                        break
+                    case .error(let error):
+                        var title: String?
+                        var message: String?
+                        switch error {
+                        case .loadingError:
+                            title = NSLocalizedString("Error", comment: "")
+                            message = NSLocalizedString("Loading error", comment: "")
+                        case .loadingLimitsError:
+                            title = NSLocalizedString("Error", comment: "")
+                            message = NSLocalizedString("Loading limits", comment: "")
+                        case .notFound:
+                            title = NSLocalizedString("Error", comment: "")
+                            message = NSLocalizedString("Owner not found", comment: "")
+                        case .mappingError:
+                            title = NSLocalizedString("Error", comment: "")
+                            message = NSLocalizedString("Mapping error", comment: "")
+                        }
+                        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                        alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: nil))
+                        self?.present(alertController, animated: true, completion: nil)
+                    }
+                    
+                    self?.reloadUI()
+                }
+            }
+        } else {
+            self.noOwnerFoundAlert()
+        }
     }
 }
 
 extension RepoViewVC: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return manager.numberOfLanguages()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 3
+        return manager.numberOfReposForLanguageAtIndex(section)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: repoCellID, for: indexPath) as? RepoCell {
             
-            
+            if let repoAtIndex = manager.repoForIndexPath(indexPath) {
+                dump(repoAtIndex)
+                cell.configure(with: repoAtIndex)
+            }
             return cell
         }
         return UITableViewCell()
     }
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40.0
+    }
+    
+    // For some style
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = self.tableView.dequeueReusableHeaderFooterView(withIdentifier: "SectionHeader") as? SectionHeader
+        
+        if let language = manager.languageAtIndex(section) {
+            let count = manager.numberOfReposForLanguageAtIndex(section)
+            let title = "\(language) (\(count))"
+            header?.sectionHeaderLabel.text = title
+        }
+        return header
     }
     
 }
