6372e13ad909a2d0789842b01c1320ae1fa7089b
Matt <mguest55@yahoo.com>
2018-03-16 17:23:01 -0700

    Added section header file and a custom xib to go along with it.

---
+++ RepoViewer/RepoViewer/View/SectionHeader.swift
@@ -0,0 +1,16 @@
+//
+//  SectionHeader.swift
+//  RepoViewer
+//
+//  Created by Matt Guest on 3/16/18.
+//  Copyright © 2018 AlphaApplications. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class SectionHeader: UITableViewHeaderFooterView {
+    
+    @IBOutlet weak var sectionHeaderLabel: UILabel!
+    
+}
